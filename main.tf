0a9fbe86a0163c44d538b86d81ce693d80b4a6ce
Medasani Charan <charanchinna646@gmail.com>
2025-06-22 18:43:49 +0530

    terraform

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ module "vpc" {
     source = "../01-terraform-aws-vpc"
     project = var.project_name
     environment = var.environment
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
 }
