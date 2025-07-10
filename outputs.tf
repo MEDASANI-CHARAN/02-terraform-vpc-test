0ae69e6f31fc206076eb24a070a22d746b71f178
Medasani Charan <charanchinna646@gmail.com>
2025-07-10 08:58:20 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
-# output "azs_info" {
-#   value = module.vpc.az_info.names
-# }
+output "azs_info" {
+  value = module.vpc.az_info.names
+}
+
+output "default_vpc" {
+  value = module.vpc.default_vpc
+}
+
+output "default_vpc_main" {
+  value = module.vpc.default_vpc_main
+}
+
